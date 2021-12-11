49448d61b228887f1eec4d4d6842b938f9b417dd
Carson Young <carson.young18@gmail.com>
2021-12-11 09:40:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit and Automate with Python')
+st.write("""This is our first Web App.
+Enjoy it!
+""")
